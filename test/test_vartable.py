764e77ca19769d65238b708b69c8472fd1cded96
averagehat <mpanciera@node22.cluster>
2019-07-25 18:11:43 -0400

    add imports should work

--- test/test_vartable.py
+++ test/test_vartable.py
@@ -1,17 +1,17 @@
 from nose.tools import eq_, ok_
-
+from vartable.types import CDS, CompoundCDS, SimpleCDS, Result, simple_translate, compound_translate, translate_all
 # might need the below
 # from pyfakefs.fake_filesystem_unittest import Patcher
 # from unittest.mock import patch
 
 
 
-def func_blah_test1():
+def func_blah_test1() -> None:
     ok_(True, "True is true")
     actual, expected = 3, 5
     eq_(3, 5, "Actual {0} is not Expected {1}".format(actual, expected))
        
     # duplicate dirs are impossible anyway, but still using Counter
-def func_blah_test2():
+def func_blah_test2() -> None:
     pass
 
