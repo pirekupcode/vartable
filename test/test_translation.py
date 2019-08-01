e0c0dee503c8da8b2df2504cf67facfa8d8b24bd
averagehat <mpanciera@node22.cluster>
2019-08-01 09:52:48 -0400

    scaffolde test file

---
+++ test/test_translation.py
@@ -0,0 +1,11 @@
+from vartable.translation import TResult, translate_one, dispatch, handle_multi_alts
+from Bio.SeqFeature import CompoundLocation, FeatureLocation, SeqFeature, ExactPosition, 
+from Bio.Seq import Seq
+from Bio.Data.CodonTable import TranslationError
+
+def translate_one_test1():
+    raise NotImplementedError()
+
+
+def translate_one_test2(): ...
+    #CompoundLocation(ExactPosition(
