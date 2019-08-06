ef3b69db9561315cb61471ffc986d39b9991c5e3
averagehat <bigmwp@gmail.com>
2019-08-06 02:55:28 -0400

    coverage tests via pytest and codecov

--- test/bin/testrunner.sh
+++ test/bin/testrunner.sh
@@ -1,4 +1,4 @@
 test/bin/run-linters.sh || true
-nosetests test/*.py
+py.test --cov=vartable test/
 vartable_report testdata/fullsample.bam.vcf --bam testdata/fullsample.bam --ref testdata/Den1__WestPac__1997.fasta --type base_caller  --mindepth 10 --minpercent 1 --out testdata/docker-test.tsv
 python -c "assert (open('testdata/docker-test.tsv').read() == open('testdata/example.tsv').read())"
