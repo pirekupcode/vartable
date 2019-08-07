51c7dff50f1292b9a1ff9e293336027ffeb3ba57
averagehat <bigmwp@gmail.com>
2019-08-06 20:04:54 -0400

    CODECOV env should work

--- test/bin/testrunner.sh
+++ test/bin/testrunner.sh
@@ -1,4 +1,5 @@
 test/bin/run-linters.sh || true
 py.test --cov=vartable test/
+codecov
 vartable_report testdata/fullsample.bam.vcf --bam testdata/fullsample.bam --ref testdata/Den1__WestPac__1997.fasta --type base_caller  --mindepth 10 --minpercent 1 --out testdata/docker-test.tsv
 python -c "assert (open('testdata/docker-test.tsv').read() == open('testdata/example.tsv').read())"
