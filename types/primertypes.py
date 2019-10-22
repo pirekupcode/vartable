0107ad795ab4064f9021ff9a1cf39ee29bf1ae46
Mike Panciera <michael.panciera.work@gmail.com>
2019-10-22 14:29:11 -0400

    Update primertypes.py

--- types/primertypes.py
+++ types/primertypes.py
@@ -20,3 +20,19 @@ class BamRead:
     def query_base_at_reference_position(position: int) -> str: ....
   
 # drawing from https://pysam.readthedocs.io/en/latest/api.html#pysam.AlignedSegment 
+
+@dataclass
+class PrimerCount:
+    alt: Alt  # position and alternate where this count came from 
+    baseATotal: int
+    baseAPrimer: int
+    baseCTotal: int
+    baseCPrimer: int
+    baseGTotal: int
+    baseGPrimer: int
+    baseTTotal: int
+    baseTPrimer: int
+    other: List[Tuple[str, int]] # e.g. N, insertion, etc
+        
+        
+def match_primers(primers: List[MappingCoordinate], alts: List[Alt], bamReads: Iterator[BamRead]) -> List[PrimerCount]: ...
