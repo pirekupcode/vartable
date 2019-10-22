aa8d190be47650f7e35fac3e771555b3192845dc
Mike Panciera <michael.panciera.work@gmail.com>
2019-10-22 14:25:14 -0400

    Update primertypes.py

--- types/primertypes.py
+++ types/primertypes.py
@@ -4,34 +4,19 @@ class Alt:
     base: str
 
 @dataclass
-class PrimerCoordinate:
+class MappingCoordinate:
     referenceStart: int
     referenceEnd: int
 
-
-# see https://pysam.readthedocs.io/en/latest/api.html#pysam.PileupColumn  for below
 @dataclass
-class PileupColumn:
-    n: int
-    nsegments: int 
-    pileups: List[PileupRead] 
-    pos: int 
-    reference_id: int 
-    reference_name: str 
-    reference_pos: int # zero-based 
-    tid: int
-
-    def get_mapping_qualities(self) -> List[int]: ...
-
-    def get_num_aligned(self) -> int: ...
-
-    def get_query_names(self) -> List[str]: ...
-
-    def get_query_positions(self) -> List[int]: ...
-
-    def get_query_qualities(self) -> List[int]: ...
-
-    def get_query_sequences(self, mark_matches: bool = False, mark_ends: bool = False, add_indels: bool = False) -> List[str]: ... #TODO check return
-
-    def __len__(self) -> int: ...
-
+class BamRead:
+    referenceEnd: int
+    referenceStart: int
+    queryStart: int
+    queryEnd: int
+    querySequence: str # without soft clipping
+    querySequenceSoftClipped: str
+        
+    def query_base_at_reference_position(position: int) -> str: ....
+  
+# drawing from https://pysam.readthedocs.io/en/latest/api.html#pysam.AlignedSegment 
