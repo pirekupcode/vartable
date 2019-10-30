b0766d100653ae5ac79c006f63f3fd47263175f2
pirekupcode <pirekupapa@gmail.com>
2019-10-30 09:52:54 -0400

    draft_test_primer_annotate.py

--- test/draft_test_primer_annotate.py
+++ test/draft_test_primer_annotate.py
@@ -1,7 +1,4 @@
 from nose.tools import eq_, ok_
-import random
-from vartable.types import CDS, CompoundCDS, SimpleCDS, \
-    Result, simple_translate, compound_translate, translate_all
 # might need the below
 # from pyfakefs.fake_filesystem_unittest import Patcher
 # from unittest.mock import patch
@@ -47,11 +44,12 @@ def match_primers(primers: List[MappingCoordinate], alts: List[Alt], bamReads: I
 
 
 #def test_primer_annotate(Alt: [str, int], consensus: , MappingCoordinate:[int, int], BamRead[int, int, int, int, str,str] PrimerCount) -> None:
-def test_match_primers(primers: List[MappingCoordinate], alts: List[Alt], bamReads: Iterator[BamRead]) -> List[PrimerCount]:
+def match_primers(primers= [MappingCoordinate(int,int)], alts= [Alt(int,str], bamReads=[BamRead(int, int,int, int, str)] -> PrimerCount: List[PrimerCount(int,int,int,int,int,int,int,int)]:
     ok_(True, "True is true")
-#Having difficulty instantiating the PrimerCount (expected instantiating is incomplete)
-    actual, expected = test_match_primers( primer: [10, 40], alt: [24, G], bamReads:[referenceEnd: 234,referenceStart:5, queryStart:9,queryEnd:154, querySequence:'ACGACGGCGCACACGGCAGCAGCAGCGACGCGGGGTTTATATATGGCACGAGCGCGCAGCGACGCAGCGCGCTTGAGACGAGGGTAGAGAGTGCGACGCCGCGCAGGAGAGGGGAGAGG' querySequenceSoftClipped:'?')
-    eq_(3, 5, "Actual {0} is not Expected {1}".format(actual, expected))
+#Having difficulty instantiating attributes for match_primer for actual and expected 
+    actual = match_primers( primer= [MappingCoordinate(10, 40)], alts= [Alt(24, 'G')], bamReads=[BamRead( 234, 5, 9, 154, 'ACGACGGCGCACACGGCAGCAGCAGCGACGCGGGGTTTATATATGGCACGAGCGCGCAGCGACGCAGCGCGCTTGAGACGAGGGTAGAGAGTGCGACGCCGCGCAGGAGAGGGGAGAGG')
+    expected = match_primers( primer= [MappingCoordinate(10, 40)], alts= [Alt(24, 'G')], bamReads=[BamRead( 234, 5, 9, 154, 'ACGACGGCGCACACGGCAGCAGCAGCGACGCGGGGTTTATATATGGCACGAGCGCGCAGCGACGCAGCGCGCTTGAGACGAGGGTAGAGAGTGCGACGCCGCGCAGGAGAGGGGAGAGG')
+        eq_(3, 5, "Actual {0} is not Expected {1}".format(actual, expected))
     result = primer_annotate(vcf, primerfile, consensus, bamfile, options)
 
     # duplicate dirs are impossible anyway, but still using Counter
